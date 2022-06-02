bf5ba4031ed204a847e522a019f0f1c2038f151a
SIARRAS Stéphane <siarras.stephane@gmail.com>
2022-06-02 03:35:33 -0400

    Récupération de données suplémentaire pour les photographes de la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,35 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const pPlaces = document.createElement('p');
+        pPlaces.textContent = city+" "+country;
+        pPlaces.setAttribute("class","Places");
+
+        const pTag = document.createElement('p');
+        pTag.textContent = tagline;
+        pTag.setAttribute("class","Tagline");
+
+        const pPrice = document.createElement('p');
+        pPrice.textContent = price+"€/jour";
+        pPrice.setAttribute("class","Price");
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pPlaces);
+        article.appendChild(pTag);
+        article.appendChild(pPrice);
+        
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {getUserCardDOM}
 }
